f76bc3cd27754100240b25765fd18221c9ed5efb
wcs <1103409364@qq.com>
2021-11-24 22:32:30 +0800

    feat: HTTP 授权
    Signed-off-by: wcs <1103409364@qq.com>

--- src/types/index.ts
+++ src/types/index.ts
@@ -30,6 +30,7 @@ export interface AxiosRequestConfig {
   xsrfHeaderName?: string
   onDownloadProgress?: (e: ProgressEvent) => void
   onUploadProgress?: (e: ProgressEvent) => void
+  auth?: AxiosBasicCredentials
 
   [propName: string]: any // 索引签名，用于 mergeConfig config1[key]
 }
@@ -137,4 +138,9 @@ export interface Cancel {
 
 export interface CancelStatic {
   new (message?: string): Cancel
+}
+
+export interface AxiosBasicCredentials {
+  username: string
+  password: string
 }
